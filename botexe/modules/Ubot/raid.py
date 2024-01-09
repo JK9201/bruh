3e4140ef86f6dcb6f2b52601c286ad2355e53adc
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-01-09 16:43:47 +0530

    Create raid.py

---
+++ botexe/modules/Ubot/raid.py
@@ -0,0 +1,26 @@
+from pyrogram import Client, filters
+from botexe.powers import get_id
+from botexe.Data.raid import *
+import random 
+from config import HANDLER as hl
+from botexe import Ubot
+
+@Ubot.on_message(filters.command("raid", hl) & filters.me)
+async def raid(client, message):
+    usage = f"`{hl}ʀᴀɪᴅ [ᴄᴏᴜɴᴛ] [ʀᴇᴘʟʏ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ]`"
+    try:
+        if message.reply_to_message:
+            count = int(message.text.split()[1])
+            id = message.reply_to_message.from_user.id
+        else:
+            count = int(message.text.split()[1])
+            txt = message.text.split()[2]
+            try:
+                id = int(txt)
+            except:
+                id = (await client.get_users(txt)).id
+    except:
+        return await message.edit(usage)
+    user = (await client.get_users(id)).mention
+    for i in range(0, count):
+        await client.send_message(message.chat.id, f" {user}"+ random.choice(RAID))
